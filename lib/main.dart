a11c524bcde322ba802f31c1ca28ea744a5dc8ae
Deflomick <deflomick@hotmail.it>
2022-03-20 01:24:47 +0100

    v2 DateFormat

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -45,14 +46,55 @@ class MyHomePage extends StatelessWidget {
           ),
           Column(children: transactions.map((tx) {
             return Card(
-              child: Text(tx.title),
+              child: Row(
+                children: <Widget> [
+                Container(
+                  margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                  ),
+                  decoration: BoxDecoration(
+                    border:Border.all(
+                    color: Colors.purple ,
+                    width: 2 ,
+                  ),
+                  ),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                 '\$${tx.amount}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                    ),
+                ),
+                ),Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget> [
+                      Text(tx.title,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                            color: Colors.green,
+                          ),),
+                      Text(
+                        DateFormat('yyyy-MM-dd').format(tx.dateTime) ,
+                        style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                          color: Colors.red,
+                      ),
+                      ),
+                  ],),
+              ],
+              ),
             );
 
           }).toList(),)
 
           ],),
-        ],
-      ),
     );
+
+
   }
 }
