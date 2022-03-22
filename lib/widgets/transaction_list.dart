5629ef151ee9c8b3390cb4eb7c9d32a571f41b00
Deflomick <deflomick@hotmail.it>
2022-03-22 21:14:38 +0100

    v3 SingleChildScrollView final

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,58 +11,58 @@ TransactionList(this.transactions);
  return Container(
    height: 300,
 
-    child: SingleChildScrollView(
-      child: Column(
 
-      children: transactions.map((tx) {
-      return Card(
-        child: Row(
-          children: <Widget> [
-            Container(
-              margin: EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 15,
-              ),
-              decoration: BoxDecoration(
-                border:Border.all(
-                  color: Colors.purple ,
-                  width: 2 ,
-                ),
-              ),
-              padding: EdgeInsets.all(10),
-              child: Text(
-                '\$${tx.amount}',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.purple,
-                ),
-              ),
-            ),Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      child: ListView.builder(
+        itemBuilder: (ctx,index){
+          return Card(
+            child: Row(
               children: <Widget> [
-                Text(tx.title,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15,
-                    color: Colors.green,
-                  ),),
-                Text(
-                  DateFormat('yyyy-MM-dd').format(tx.dateTime) ,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15,
-                    color: Colors.red,
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
                   ),
-                ),
-              ],),
-          ],
-        ),
-      );
+                  decoration: BoxDecoration(
+                    border:Border.all(
+                      color: Colors.purple ,
+                      width: 2 ,
+                    ),
+                  ),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    '\$${transactions[index].amount}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
+                    ),
+                  ),
+                ),Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget> [
+                    Text(transactions[index].title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                        color: Colors.green,
+                      ),),
+                    Text(
+                      DateFormat('yyyy-MM-dd').format(transactions[index].dateTime) ,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],),
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
 
-    }).toList(),
       ),
-  ),
+
  );
   }
 }
