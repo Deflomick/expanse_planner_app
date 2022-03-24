31f0fede238caf483dd9b433cb5ec6929ab54e3c
Deflomick <deflomick@hotmail.it>
2022-03-24 19:20:14 +0100

    v1 floatingbutton

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,12 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
+        actions: <Widget>[
+          IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.add),
+          ),
+        ],
       ),
       body:  SingleChildScrollView(
         child: Column(
@@ -51,6 +57,11 @@ class MyHomePage extends StatelessWidget {
 
       ],
     ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){},
       ),
     );
 
